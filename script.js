1023f020dc7c6c5a94290e5a70262c8d370b66aa
Milos0213 <milospavla002@gmail.com>
2023-06-17 01:24:54 +0200

    Added 16x16 grid, paint function for squares All elements inside of a container are made from javascript. Grid made in two dimensions, outer containing rows which are on display flex. Every row contains inner dimension of squares (same height as width and margin on 1px). Square triggers paint function on mouse hover that paints it black. Rows are positioned vertically thanks to flex-direction which is set to column on container div. Plan is to implement grid size selection, buttons for changing colors, reset and eraser.

---
+++ script.js
@@ -0,0 +1,21 @@
+function paint() {  // Adds color of square on mouse hover
+    this.style.backgroundColor = 'black';
+}
+
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 16; i ++) {   // Outter FOR, creating rows
+    let row = document.createElement('row');
+    row.style.display = 'flex';
+    container.appendChild(row);
+
+    for(let j = 0; j < 16; j ++) {  // Inner FOR, creating 16 squares for every i iteration (row)
+        let square = document.createElement('div');
+        square.style.height = '35px';
+        square.style.width = '35px';
+        square.style.backgroundColor = '#fff';
+        square.style.margin = '1px';
+        square.addEventListener('mouseover', paint);  // Adds paint function listener on mouseover
+        row.appendChild(square);
+    }
+}
